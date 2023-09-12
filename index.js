60d2fe3160cddcc9201bdcf1f4e70072912f9862
Yulia Rose <makomitsuki@gmail.com>
2023-09-12 16:00:21 +0100

    attempt 2

--- index.js
+++ index.js
@@ -555,7 +555,7 @@ client.on('interactionCreate', async interaction => {
 			}
 
 			await interaction.reply(`Your next scheduled jolli-meeting is at ${hammerTimeHelper(nextMeeting, 'F')} which is ${hammerTimeHelper(nextMeeting, 'R')} from now \n\n
-				The following meeting after that won't be till ${hammerTimeHelper(ffMeeting.nextInvocation(), 'F')} which is ${hammerTimeHelper(ffMeeting, 'R')} from now `);
+				The following meeting after that won't be till ${hammerTimeHelper(ffMeeting, 'F')} which is ${hammerTimeHelper(ffMeeting, 'R')} from now `);
 		}
 		else {
 			await interaction.reply(`**STOP RIGHT THERE!** You're not allowed to see that!`);
