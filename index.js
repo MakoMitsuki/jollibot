7942eb3060f9117db22bb376342b8c0169d3a19b
Yulia Rose <makomitsuki@gmail.com>
2025-02-14 13:07:23 +0000

    test react

--- index.js
+++ index.js
@@ -654,6 +654,13 @@ client.on('message', async msg => {
 	}
 });
 
+// https://discord.js.org/docs/packages/discord.js/14.14.1/Events:Enum#/docs/discord.js/main/class/Client?scrollTo=e-messageReactionRemove
+client.on('messageReactionRemove', (reaction, user) => {
+    if (reaction.message.channel.id === '465932520266137600') {
+        client.channels.cache.get('1082385963675811921').send(`${user.username} removed their "${reaction.emoji.name}" reaction at ${reaction.message.id}`);
+    }
+});
+
 client.on('interactionCreate', async interaction => {
 	try {
 		if (!interaction.isChatInputCommand() && !interaction.isButton()) return;
